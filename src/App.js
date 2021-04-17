16f11d79673cfb0575977f0a653d6d0dcb69ff9f
Phung Phu <phungphu@uw.edu>
2021-04-17 15:36:16 -0700

    add weather.csv

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from "react";
 const App = () => {
     return (
         <div>
-
+            {/* <svg>
+                <circle cx={20} cy={20} r="5" />
+            </svg> */}
         </div>
     );
 }
